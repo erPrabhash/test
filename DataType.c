0e236c1dcd677f05a1ef87a2857ed05c5f2c0034
erPrabhash <32672276+erPrabhash@users.noreply.github.com>
2018-07-14 01:47:05 -0700

    Update DataType.c

--- DataType.c
+++ DataType.c
@@ -3,7 +3,7 @@
  {
     int x= 0b10;
     printf("\nValue  of x: %d dec and 0o%o oct and ox%x \a",x,x,x,x);
-printf("New updates");
+    printf("New updates___ updet through Git Hub");
 
        printf("\n%c%c%c%C",176,177,178,219);
  }
